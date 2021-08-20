ea81cddd2930631a543447a4cce2da8e13342412
Katie DeMet <katie.demet7@gmail.com>
2021-08-20 12:55:58 -0500

    extra credit finished post deadline

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+//Unique Characters
+
 const sampleString1 = "repeated letters"
 const sampleString2 = "should"
 const sampleString3 = "setters and getters"
@@ -22,3 +24,24 @@ console.log(uniqueLetters(sampleString1))
 console.log(uniqueLetters(sampleString2))
 console.log(uniqueLetters(sampleString3))
 console.log(uniqueLetters(sampleString4))
+
+//Sum Zero
+
+let testArr = [1, 2, 3, -2]
+let testArr2 = [2, 3, 5, 7, 9]
+let testArr3 = [-3, 10, 3, -5]
+
+function includesSumZero(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = 0; j < arr.length; j++) {
+            if(arr[i]+arr[j] === 0) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+console.log(includesSumZero(testArr))
+console.log(includesSumZero(testArr2))
+console.log(includesSumZero(testArr3))
