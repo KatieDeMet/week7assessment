911537d3678fc8e8fdc8df9332e68a7505ca76b2
Katie DeMet <katie.demet7@gmail.com>
2021-08-20 12:12:50 -0500

    post deadline update of extra credit

--- test.js
+++ test.js
@@ -1,22 +1,24 @@
 const sampleString1 = "repeated letters"
 const sampleString2 = "should"
+const sampleString3 = "setters and getters"
+const sampleString4 = "wonderful"
 
 function uniqueLetters(str) {
     let uniqueLetter = ""
+    let stringToTest = ""
     for(let i = 0; i < str.length; i++) {
       uniqueLetter = str[i]
-      console.log(uniqueLetter)
-      str.replace("s", "t")
-      console.log(str)
-      if(str.includes(uniqueLetter)) {
+    //   console.log(uniqueLetter)
+      stringToTest = str.replace(uniqueLetter, "")
+    //   console.log(stringToTest)
+      if(stringToTest.includes(uniqueLetter)) {
           return false
       }
     }
     return true
 }
 
-// console.log(uniqueLetters(sampleString1))
+console.log(uniqueLetters(sampleString1))
 console.log(uniqueLetters(sampleString2))
-console.log(sampleString2.replace("s", "t"))
-
-//could not get .replace to work
+console.log(uniqueLetters(sampleString3))
+console.log(uniqueLetters(sampleString4))
